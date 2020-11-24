42a8973adb96ae00bc8c4f9c6bda1a84d168a122
Marcus Bartlett <mabartlett@hotmail.com>
2020-11-24 09:58:06 -0800

    Created default package and MazeMain class.

---
+++ src/MazeMain.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * This class contains the main method for the Trivia Maze program.
+ * @author Team 2 (Calvin Nguyen, Joseph Bode, and Marcus Bartlett)
+ * @version Fall 2020
+ */
+public class MazeMain {
+
+	/**
+	 * The main method.
+	 * @param args an array of Strings passed by command-line input.
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
